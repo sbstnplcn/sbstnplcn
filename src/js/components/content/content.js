bf7a063fbb87d7ef1dbb088b8aa937083a9b3e29
sbstnplcn <sbstnplcn@gmail.com>
2016-11-23 23:58:49 +0100

    add json and edit content exp and studies

--- src/js/components/content/content.js
+++ src/js/components/content/content.js
@@ -4,8 +4,22 @@
         controller: ['$http', function($http) {
             angular.extend(this, {
                 $onInit() {
-
+                    $http.get('src/js/components/content/exp-pro.json').then((res) => {
+                        this.exps = res.data
+                    })
+                    $http.get('src/js/components/content/studies.json').then((res) => {
+                        this.studies = res.data
+                    })
+                    this.expslimit = 3
+                    this.loadMoreExps = () => {
+                        this.expslimit += 1
+                    }
+                    this.studieslimit = 3
+                    this.loadMoreStudies = () => {
+                        this.studieslimit += 1
+                    }
                 }
+
             })
         }]
     })
